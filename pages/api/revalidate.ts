59e29284dc055ab7acf9d1c119a996a93ed91fa1
Nick DeJesus <ndejesus1227@gmail.com>
2022-02-25 10:25:24 -0500

    update revalidate

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,44 +1,49 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import * as prismicH from "@prismicio/helpers";
+import { NextApiRequest, NextApiResponse } from 'next';
 
-import { createClient } from "../../prismicio";
+import * as prismic from '@prismicio/client';
+import * as prismicH from '@prismicio/helpers';
+
+import { createClient } from '../../prismicio';
 
 /**
  * This API endpoint will be called by a Prismic webhook. The webhook
  * will send an object containing a list of added, updated, or deleted
  * documents. Pages for those documents will be rebuilt.
+ *
+ * The Prismic webhook must send the correct secret.
  */
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.body.type === "api-update" && req.body.documents.length > 0) {
+  if (req.body.type === 'api-update' && req.body.documents.length > 0) {
+    // Check for secret to confirm this is a valid request
+    if (req.body.secret !== process.env.PRISMIC_WEBHOOK_SECRET) {
+      return res.status(401).json({ message: 'Invalid token' });
+    }
+
     // If you have a `createClient()` function defined elsewhere in
     // your app, use that instead
-    const client = createClient();
+    const client = prismic.createClient('your-repo-name');
 
     // Get a list of URLs for any new, updated, or deleted documents
     const documents = await client.getAllByIDs(req.body.documents);
     const urls = documents.map((doc) => prismicH.asLink(doc));
 
-    console.log("revalidate.ts - ", { urls });
-
     try {
       // Revalidate the URLs for those documents
       await Promise.all(
-        urls
-          .filter((url): url is string => url !== null)
-          .map(async (url) => await res.unstable_revalidate(url))
+        urls.map(async (url) => await res.unstable_revalidate(url as string))
       );
 
       return res.json({ revalidated: true });
     } catch (err) {
       // If there was an error, Next.js will continue to show
       // the last successfully generated page
-      return res.status(500).send("Error revalidating");
+      return res.status(500).send('Error revalidating');
     }
   }
 
   // If the request's body is unknown, tell the requester
-  return res.status(400).json({ message: "Invalid body" });
+  return res.status(400).json({ message: 'Invalid body' });
 }
