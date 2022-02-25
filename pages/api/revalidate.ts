cf55d14d5bf4fac5290b81fd01d44983b202f364
Nick DeJesus <ndejesus1227@gmail.com>
2022-02-25 10:48:01 -0500

    switching back

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,6 +1,4 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-
-import * as prismic from '@prismicio/client';
 import * as prismicH from '@prismicio/helpers';
 
 import { createClient } from '../../prismicio';
@@ -9,31 +7,28 @@ import { createClient } from '../../prismicio';
  * This API endpoint will be called by a Prismic webhook. The webhook
  * will send an object containing a list of added, updated, or deleted
  * documents. Pages for those documents will be rebuilt.
- *
- * The Prismic webhook must send the correct secret.
  */
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.body.type === 'api-update' && req.body.documents.length > 0) {
-    // Check for secret to confirm this is a valid request
-    if (req.body.secret !== process.env.PRISMIC_WEBHOOK_SECRET) {
-      return res.status(401).json({ message: 'Invalid token' });
-    }
-
     // If you have a `createClient()` function defined elsewhere in
     // your app, use that instead
-    const client = prismic.createClient('your-repo-name');
+    const client = createClient();
 
     // Get a list of URLs for any new, updated, or deleted documents
     const documents = await client.getAllByIDs(req.body.documents);
     const urls = documents.map((doc) => prismicH.asLink(doc));
 
+    console.log('revalidate.ts - ', { urls });
+
     try {
       // Revalidate the URLs for those documents
       await Promise.all(
-        urls.map(async (url) => await res.unstable_revalidate(url as string))
+        urls
+          .filter((url): url is string => url !== null)
+          .map(async (url) => await res.unstable_revalidate(url))
       );
 
       return res.json({ revalidated: true });
